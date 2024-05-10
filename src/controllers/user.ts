bc6ba6c5a1dea598dbd34a807185e29b2d91ff06
shivang-16 <shivangyadav121@gmail.com>
2024-05-10 22:45:39 +0530

    added generateOTP

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,5 @@
 import {Request, Response, NextFunction } from "express";
-import User from "../models/userModel";
+import Mentor from "../models/userModel";
 import { CustomError } from "../middlewares/error";
 
 export const register = async(req: Request, res: Response, next: NextFunction) =>{
