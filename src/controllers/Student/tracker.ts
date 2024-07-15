87dc20f084313887d77df529a8359ff2c93ebdf3
shivang-16 <shivangyadav121@gmail.com>
2024-07-16 00:31:43 +0530

    update get tracker

--- src/controllers/Student/tracker.ts
+++ src/controllers/Student/tracker.ts
@@ -2,13 +2,14 @@ import { Request, Response, NextFunction } from "express";
 import moment from "moment-timezone";
 import { db } from "../../db/db";
 import { CustomError } from "../../middlewares/error";
+import mongoose from "mongoose";
 
 export const getStudentTracker = async (req: Request, res: Response, next: NextFunction) => {
     try {
 
     const Tracker = db.collection("trackers")
    
-      const tracker = await Tracker.find({ user: req.params.id, "subject.name": req.query.subject });
+      const tracker = await Tracker.find({ user: new mongoose.Types.ObjectId(req.params.id), "subject.name": req.query.subject }).toArray();
   
       if (!tracker) {
         return res.status(404).json({
@@ -19,7 +20,7 @@ export const getStudentTracker = async (req: Request, res: Response, next: NextF
   
       return res.status(200).json({
         success: true,
-        tracker,
+        data: tracker,
       });
     } catch (error: any) {
       next(new CustomError(error.message));
