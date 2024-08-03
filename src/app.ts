13a7cadd4c3ff3ca201563a164299bf33da9d708
shivang-16 <shivangyadav121@gmail.com>
2024-08-03 22:33:50 +0530

    update deployment file

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,7 @@ app.use(express.json())
 app.use(urlencoded({extended: true}))
 app.use(
     cors({
-      origin: [process.env.FRONTEND_URL!, 'https://mentor.leadlly.in', "http://localhost:3000"],
+      origin: [process.env.FRONTEND_URL!, "http://localhost:3000"],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
       credentials: true,
     }),
